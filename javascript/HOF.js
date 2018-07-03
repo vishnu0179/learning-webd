95b5aeac715d5c91a8bc109e99b538809f6ef904
vishnu0179 <34721061+vishnu0179@users.noreply.github.com>
2018-07-03 21:39:19 +0530

    practical example added

--- javascript/HOF.js
+++ javascript/HOF.js
@@ -8,3 +8,30 @@ function creategreeter(name) {
 
 vishnugreeter=creategreeter('Vishnu');
 vishnugreeter();
+
+
+let a= function() {}
+let b=a
+let c=b
+
+
+
+function createCounter(initVal, delta){
+    let val= initVal
+    function counter(){
+        val=val+delta
+        return val;
+    }
+    return counter;
+}
+ 
+count1=createCounter(3,3);
+count2 = createCounter(5,5);
+
+console.log(count1());
+console.log(count1());
+console.log(count1());
+
+console.log(count2());
+console.log(count2());
+console.log(count2());
