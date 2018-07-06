4eb474a71ab2f82eca48bd71d3418ef86a684642
vishnu0179 <34721061+vishnu0179@users.noreply.github.com>
2018-07-06 05:55:06 +0530

    new keyword

---
+++ javascript/objects.js
@@ -0,0 +1,15 @@
+function fruit (color, taste)
+{
+    this.color = color;
+    this.taste = taste;
+}
+let orange= new fruit("orange","sweet")
+let apple = new fruit("red","Sour")
+
+console.log(orange)
+console.log(apple)
+
+let mango={
+    color:"yellow",
+    taste:"sweet"
+}
